fc5d315e647e7c0360e1b569083d5bef80a5aaeb
Hannah <Viking8@gmx.de>
2020-11-02 18:11:05 +0100

    add functions to play sound files

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 console.log('hello test');
 
-document.querySelector('.btn').addEventListener('click', () => {
-    console.log('click button');
+const trumpet = document.getElementById("trumpet");
+   
+function playAudio() { 
+    trumpet.play(); 
+} 
 
-})
+function pauseAudio() { 
+    trumpet.pause(); 
+} 
+
+const tkazoo = document.getElementById("kazoo");
+
+function playAudio() {
+  kazoo.play();
+}​
+
+function pauseAudio() {
+  kazoo.pause();
+}​
+
+// const anotherSound = document.getElementById("another-sound");​
+
+// function playAudio() {
+//   anotherSound.play();
+// }​
+
+// function pauseAudio() {
+//   anotherSound.pause();
+// }
+
